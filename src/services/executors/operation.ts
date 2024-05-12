46a0436b24671aaf66cf8393fa61c789df1c1a36
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2024-05-12 09:31:57 +0700

    refactor: improve execute operation

--- src/services/executors/operation.ts
+++ src/services/executors/operation.ts
@@ -5,18 +5,44 @@ import executeAddition from './addition'
 import executeMultiplication from './multiplication'
 import executeSubtraction from './subtraction'
 
+/**
+ * A function type that defines a signature for executing mathematical operations on two matrices.
+ * This type is used to abstract the operation execution, allowing the specific operation (addition, subtraction,
+ * multiplication) to be dynamically selected and executed.
+ *
+ * @param leftOperand - The left matrix operand in the operation.
+ * @param rightOperand - The right matrix operand in the operation.
+ * @returns A Matrix resulting from the operation.
+ */
+type Executor = (leftOperand: Matrix, rightOperand: Matrix) => Matrix
+
+/**
+ * Executes a mathematical operation on two matrix operands based on the specified operator in the Operation object.
+ * This function routes the operation to the appropriate function based on whether it is an addition, subtraction,
+ * or multiplication operation.
+ *
+ * @param operation - An object describing the operation to be executed, including the operator and matrix operands.
+ * @returns A Matrix resulting from the executed operation.
+ */
 const executeOperation = (operation: Operation): Matrix => {
+  /** Extracts the operator from the operation object to determine the specific execution path. */
   const operator = operation.operator
 
+  /**
+   * A variable holding a function reference to the specific mathematical operation to be executed,
+   * determined based on the operator type. This function adheres to the Executor type signature.
+   */
+  let execute: Executor
+
   if (operator === Operator.ADDITION) {
-    return executeAddition(operation.leftOperand, operation.rightOperand)
+    execute = executeAddition
+  } else if (operator === Operator.SUBTRACTION) {
+    execute = executeSubtraction
+  } else {
+    execute = executeMultiplication
   }
 
-  if (operator === Operator.SUBTRACTION) {
-    return executeSubtraction(operation.leftOperand, operation.rightOperand)
-  }
-
-  return executeMultiplication(operation.leftOperand, operation.rightOperand)
+  return execute(operation.leftOperand, operation.rightOperand)
 }
 
 export default executeOperation
