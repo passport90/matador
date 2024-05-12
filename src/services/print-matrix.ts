9bb0420065d0bdbf80d9522db6fc1af62a6bd78b
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2024-05-12 08:48:01 +0700

    feat: print matrix

--- src/services/print-matrix.ts
+++ src/services/print-matrix.ts
@@ -1,9 +1,6 @@
 import Matrix from '../types/matrix'
 import Printer from '../types/printer'
 
-const printMatrix = (matrix: Matrix, printer: Printer): void => {
-  matrix
-  printer
-}
+const printMatrix = (matrix: Matrix, print: Printer): void => print(JSON.stringify(matrix.values))
 
 export default printMatrix
---
+++ tests/services/print-matrix.test.ts
@@ -0,0 +1,28 @@
+import Matrix from '../../src/types/matrix'
+import printMatrix from '../../src/services/print-matrix'
+
+describe('printMatrix', () => {
+  it('JSON-stringify matrix values and print them', () => {
+    // Prepare
+    const matrix: Matrix = {
+      values: [
+        [8, 4, -1],
+        [5, 2, 3],
+        [4, -2, 7],
+        [10, 5, -2],
+      ],
+      rowCount: 4,
+      columnCount: 3,
+    }
+
+    const printer = jest.fn()
+
+    // Execute
+    printMatrix(matrix, printer)
+
+    // Assert
+    const expectedSerialization = '[[8,4,-1],[5,2,3],[4,-2,7],[10,5,-2]]'
+    expect(printer).toHaveBeenCalledTimes(1)
+    expect(printer).toHaveBeenCalledWith(expectedSerialization)
+  })
+})
